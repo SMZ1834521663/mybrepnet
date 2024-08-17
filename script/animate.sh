e6f1772270d70a225642575988e22b616ac37e05
”SMZ1834521663” <1834521663@qq.com>
2024-08-17 19:41:56 +0800

    test

--- script/animate.sh
+++
@@ -1,5 +0,0 @@
-python animate.py mod=test exp_name=zju377 test_novel_pose=True subject=zju377_animate ratio=0.5 gpus=0 animate=True
-# python animate.py mod=test exp_name=zju377 test_novel_pose=True subject=zju377_animate ratio=0.5 gpus=0 animate=True
-# python animate.py mod=test exp_name=zju377 test_novel_pose=True subject=zju377_animate ratio=0.5 gpus=0 animate=True
-# python animate.py mod=test exp_name=zju377 test_novel_pose=True subject=zju377_animate ratio=0.5 gpus=0 animate=True
-# python animate.py mod=test exp_name=zju377 test_novel_pose=True subject=zju377_animate ratio=0.5 gpus=0 animate=True
--- script/train_script1.sh
+++
@@ -1,4 +0,0 @@
-
-
-python train_net.py exp_name=hash_grid ratio=0.5 subject=zju377 gpus=1 hash_grid=True pose_correction=True resume=False 
-python train_net.py exp_name=hash_grid ratio=0.5 subject=zju377 gpus=1 hash_grid=True pose_correction=True resume=False mod=test check_ckpt=True
